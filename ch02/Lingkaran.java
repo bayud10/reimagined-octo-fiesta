52af8a5dd9b96f9882ec3d81e5ad04aa56aa8c46
bayud10 <bayu.dwinata@ymail.com>
2020-04-18 22:38:42 +0700

    First time commit after adding everything

---
+++ ch02/Lingkaran.java
@@ -0,0 +1,7 @@
+package ch02;
+public class Lingkaran {
+double jariJari;
+double luas() {
+return Math.PI * jariJari * jariJari;
+}
+}
---
+++ ch02/Persegi.java
@@ -0,0 +1,7 @@
+package ch02;
+public class Persegi {
+int sisi;
+int luas() {
+return sisi*sisi;
+}
+}
